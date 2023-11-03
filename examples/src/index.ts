05394ff662e636513a64f8e7ca8080f5ce5ac449
inaridiy <inainainariinainari@gmail.com>
2023-11-03 15:09:48 +0900

    demo

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,7 +1,10 @@
+import { promises as fs } from "fs";
 import { htmlToMarkdown } from "webforai";
 import { loadHtml } from "webforai/loaders/playwright";
 
 const url = "https://www.google.com/search?q=webforai";
 const html = await loadHtml(url);
 
-const markdown = htmlToMarkdown(html, url);
+const markdown = htmlToMarkdown(html, { solveLinks: url });
+
+await fs.writeFile("output.md", markdown);
