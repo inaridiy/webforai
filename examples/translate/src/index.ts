a4c08e2eb4a7a81508c706dd55224e83fc022b7c
inaridiy <inainainariinainari@gmail.com>
2024-03-20 10:45:25 +0900

    feat: update translation prompt

--- examples/translate/src/index.ts
+++ examples/translate/src/index.ts
@@ -23,18 +23,19 @@ await fs.writeFile(`.output/${fileName}.md`, url);
 const html = await loadHtml(url);
 const mdast = htmlToMdast(html);
 
-const splitted = await mdastSplitter(mdast, async (md) => 1000 > md.length);
+const splitted = await mdastSplitter(mdast, async (md) => 2000 > md.length);
 
 for (const mdast of splitted) {
 	const baseMd = mdastToMarkdown(mdast);
 	if (baseMd.length < 10) continue;
-	const prompt = `あなたは、Markdownドキュメント翻訳のスペシャリストです。次のWebサイトをMarkdownに変換したドキュメントを、日本語に翻訳してください。
+	const prompt = `あなたは、Markdownドキュメントを翻訳するスペシャリストです。次のWebサイトをMarkdownに変換したドキュメントを、日本語に翻訳してください。
 
 <instruction>
 1. 翻訳する際は、推敲を重ね、あたかも最初から日本語の文章であるかのように翻訳してください。
 2. 専門分野の翻訳では、その分野の用語を正しく理解し、適切な訳語をあててください。
 3. ドキュメントは機械的に変換したものであるため、不整合や誤り、本文以外の不要な情報が含まれている可能性があります。それらの誤りを修正して読みやすい形にしてください。
-4. 翻訳したMarkdownのみを出力してください。
+4. まず翻訳に移る前に、文章をよく読んで<thinking></thinking>タグ内で内容のメモや翻訳の方針を立ててください。
+5. その後、<translation></translation>タグ内に翻訳したMarkdownを記入してください。
 </instruction>
 
 <markdown>
@@ -47,6 +48,6 @@ ${baseMd}
 		messages: [{ role: "user", content: prompt }],
 	});
 
-	const translatedMd = `${translatedRes.content[0].text.trim()}\n`;
+	const translatedMd = `${translatedRes.content[0].text.trim().match(/<translation>([\s\S]+)<\/translation>/)?.[1]}\n`;
 	await fs.appendFile(`.output/${fileName}.md`, translatedMd);
 }
