028a6bced1915faffcad9878deff98440a51a5cb
yutakobaayshidev <hi@yutakobayashi.dev>
2024-03-22 23:21:46 +0900

    chore: sleep

--- packages/webforai/src/loaders/cf-puppeteer.ts
+++ packages/webforai/src/loaders/cf-puppeteer.ts
@@ -1,11 +1,15 @@
 import puppeteer from "@cloudflare/puppeteer";
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const loadHtml = async (url: string, ctx: puppeteer.BrowserWorker) => {
 	const browser = await puppeteer.launch(ctx);
 	const page = await browser.newPage();
 	await page.goto(url);
 
 	const html = await page.content();
+
+	await Promise.race([page.waitForNetworkIdle(), sleep(10000)]);
 
 	await page.close();
 
