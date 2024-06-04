59738b7be944dd2f59248423d72c8f8a8aee78c1
inaridiy <inainainariinainari@gmail.com>
2024-06-05 02:35:13 +0900

    test: simplest test

---
+++ vitest.config.ts
@@ -0,0 +1,7 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+
+// biome-ignore lint/style/noDefaultExport: This is a configuration file
+export default defineConfig({
+	test: {},
+});
