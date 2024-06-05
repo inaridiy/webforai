91942c4ddb3949b83e8b2e23b9833ed1c80fdf6b
inaridiy <inainainariinainari@gmail.com>
2024-06-06 01:23:06 +0900

    docs: add playwright loader docs

--- packages/webforai/src/loaders/playwright.ts
+++ packages/webforai/src/loaders/playwright.ts
@@ -1,10 +1,18 @@
 import { type Browser, type BrowserContext, chromium } from "playwright";
 
 /**
- * Useful function for quick prototyping. **Not recommended** for use in production environments.
+ * Useful function for load the HTML of a URL using Playwright.
+ * **Not recommended** for use in production environments.
  * @param url - The URL to load.
  * @param context - The Playwright browser context to use. If not provided, a new browser context will be created and closed after loading the URL.
  * @returns The HTML content of the URL.
+ * @example
+ * ```ts
+ * import { loadHtml } from "webforai/loaders/playwright";
+ *
+ * const html = await loadHtml("https://example.com");
+ * console.log(html);
+ * ```
  */
 export const loadHtml = async (url: string, context?: BrowserContext | Browser) => {
 	const _context = context ?? (await chromium.launch({ headless: true }));
