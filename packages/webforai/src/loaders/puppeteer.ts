9907169c4fc150cda832478c0d752e96812803f1
moons <moons@moons14.com>
2024-06-09 23:40:39 +0900

    Add loader using puppeteer

---
+++ packages/webforai/src/loaders/puppeteer.ts
@@ -0,0 +1,23 @@
+import puppeteer from "puppeteer";
+import type { PuppeteerLaunchOptions } from "puppeteer";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const loadHtml = async (url: string, ctx?: PuppeteerLaunchOptions) => {
+	const browser = await puppeteer.launch(
+		ctx || {
+			headless: true,
+			args: ["--no-sandbox", "--disable-setuid-sandbox"],
+		},
+	);
+	const page = await browser.newPage();
+	await page.goto(url);
+
+	const html = await page.content();
+
+	await Promise.race([page.waitForNetworkIdle(), sleep(10000)]);
+
+	await page.close();
+
+	return html;
+};
