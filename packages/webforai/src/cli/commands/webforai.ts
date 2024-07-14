e8b64f344daccc1d59b40003dd247184110571d8
inaridiy <inainainariinainari@gmail.com>
2024-07-15 00:42:51 +0900

    feat: refactor

--- packages/webforai/src/cli/commands/webforai.ts
+++ packages/webforai/src/cli/commands/webforai.ts
@@ -1,20 +1,101 @@
+import fs from "node:fs";
+import path from "node:path";
+import { spinner as clackSpinner, spinner } from "@clack/prompts";
+import pc from "picocolors";
+import { htmlToMarkdown } from "../../html-to-markdown";
 import { inputOutputPath } from "../helpers/inputOutputPath";
 import { inputSourcePath } from "../helpers/inputSourcePath";
+import { requirePackage } from "../helpers/requirePackage";
 import { selectExtractMode } from "../helpers/selectExtractMode";
 import { selectLoader } from "../helpers/selectLoader";
 import { isUrl } from "../utils";
+
+const aiModeOptions = { linkAsText: true, tableAsText: true, hideImage: true };
+const readabilityModeOptions = { linkAsText: false, tableAsText: false, hideImage: false };
+
+type LoaderOptions = { type: "web"; loader: string } | { type: "local"; loader: null };
+
+const loadHtml = async (sourcePath: string, loader: LoaderOptions) => {
+	const loadSpinner = clackSpinner();
+
+	if (loader.type === "local") {
+		loadSpinner.start("Retrieving content.");
+		if (!fs.existsSync(sourcePath)) {
+			console.error(pc.red("The file does not exist"));
+			process.exit(1);
+		}
+
+		const stats = fs.statSync(sourcePath);
+		if (!stats.isFile()) {
+			console.error(pc.red("The source must be a file"));
+			process.exit(1);
+		}
+
+		const content = fs.readFileSync(sourcePath, "utf-8");
+		loadSpinner.stop("Content retrieval is complete!");
+		return content;
+	}
+
+	loadSpinner.start("Downloading content.");
+
+	if (loader.loader === "playwright") {
+		await requirePackage("playwright");
+		const { loadHtml } = await import("../../loaders/playwright");
+		const content = await loadHtml(sourcePath);
+		loadSpinner.stop("Content download is complete!");
+		return content;
+	}
+	if (loader.loader === "puppeteer") {
+		await requirePackage("puppeteer");
+		const { loadHtml } = await import("../../loaders/puppeteer");
+		const content = await loadHtml(sourcePath);
+		loadSpinner.stop("Content download is complete!");
+		return content;
+	}
+
+	const { loadHtml } = await import("../../loaders/fetch");
+
+	const content = await loadHtml(sourcePath);
+	loadSpinner.stop("Content download is complete!");
+	return content;
+};
 
 export const webforaiCommand = async (
 	initialPath: string,
 	initialOutputPath: string,
 	options: { mode?: string; loader: string; debug?: boolean; baseUrl?: string; stdout?: boolean },
-	// biome-ignore lint/complexity/noExcessiveCognitiveComplexity: <explanation>
 ) => {
 	const sourcePath = await inputSourcePath(initialPath);
 
-	const loader = isUrl(sourcePath) && (await selectLoader(options.loader));
+	const loader: LoaderOptions = isUrl(sourcePath)
+		? { type: "web", loader: await selectLoader(options.loader) }
+		: { type: "local", loader: null };
 
-	const outputPath = await inputOutputPath(sourcePath, initialOutputPath);
+	const outputPath = options.stdout ? await inputOutputPath(sourcePath, initialOutputPath) : null;
 
 	const mode = await selectExtractMode(options.mode);
+
+	const content = await loadHtml(sourcePath, loader);
+
+	const convertSpinner = spinner();
+	convertSpinner.start("Converting content to markdown.");
+
+	const markdown = htmlToMarkdown(content, {
+		baseUrl: isUrl(sourcePath) ? sourcePath : options.baseUrl,
+		...(mode === "ai" ? aiModeOptions : readabilityModeOptions),
+	});
+
+	if (!outputPath) {
+		console.info(markdown);
+		return;
+	}
+
+	const directory = path.dirname(outputPath);
+
+	if (!fs.existsSync(directory)) {
+		fs.mkdirSync(directory, { recursive: true });
+	}
+	fs.writeFileSync(outputPath, markdown);
+
+	convertSpinner.stop("Markdown conversion is complete!");
 };
---
+++ packages/webforai/src/cli/helpers/requirePackage.ts
@@ -0,0 +1,36 @@
+import { execSync } from "node:child_process";
+import { confirm } from "@clack/prompts";
+import { assertContinue } from "./assertContinue";
+
+const isPackageInstalled = async (packageName: string) => {
+	try {
+		await import(packageName);
+		return true;
+	} catch {
+		return false;
+	}
+};
+
+export const requirePackage = async (packageName: string) => {
+	if (await isPackageInstalled(packageName)) {
+		return true;
+	}
+
+	const isInstall = await confirm({
+		message: `The package ${packageName} is not installed. Do you want to install it?`,
+		initialValue: true,
+	});
+
+	assertContinue(isInstall);
+
+	if (!isInstall) {
+		console.error("The package is required to continue.");
+		process.exit(1);
+	}
+
+	const installCommand = `npm install ${packageName} --no-save`;
+	console.info(`Installing ${packageName}...`);
+	execSync(installCommand, { stdio: "inherit" });
+
+	return true;
+};
