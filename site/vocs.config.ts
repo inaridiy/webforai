49085ee9f271e231d2e02e29770d74a73f611b92
inaridiy <inainainariinainari@gmail.com>
2024-10-12 02:33:37 +0900

    feat:

--- site/vocs.config.ts
+++ site/vocs.config.ts
@@ -5,8 +5,16 @@ export default defineConfig({
 	title: "Webforai",
 	sidebar: [
 		{
+			text: "Installation",
+			link: "/installation",
+		},
+		{
 			text: "Getting Started",
 			link: "/getting-started",
+		},
+		{
+			text: "How it works",
+			link: "/how-it-works",
 		},
 	],
 });
