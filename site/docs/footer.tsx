b1e354c8d73acafbff22be988f646c92b448fbbf
inaridiy <inainainariinainari@gmail.com>
2024-10-20 00:30:50 +0900

    feat(site): footer

---
+++ site/docs/footer.tsx
@@ -0,0 +1,9 @@
+// biome-ignore lint/style/noDefaultExport:
+export default function Footer() {
+	return (
+		<div className="items-center flex flex-col mt-12 w-full">
+			<div>Released under the MIT License.</div>
+			<div>Copyright © 2024-present inaridiy and contributors.</div>
+		</div>
+	);
+}
